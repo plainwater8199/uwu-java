d6b7db1452f7b8d697abc214a43570656c02dc73
water <water.wang@autogeneral.cn>
2023-11-02 06:27:36 +0800

    redis-test

--- uwu-study/uwu-study-db/src/main/java/com/uwu/study/db/test/TestDBController.java
+++ uwu-study/uwu-study-db/src/main/java/com/uwu/study/db/test/TestDBController.java
@@ -1,17 +1,24 @@
 package com.uwu.study.db.test;
 
+//import com.uwu.study.redis.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @Slf4j
 public class TestDBController {
+
+//    @Resource
+//    private RedisUtil redisUtil;
+
     @RequestMapping(value = "/testDBCall")
     public void apiCall(HttpServletRequest request, HttpServletResponse response){
+//        System.out.println(redisUtil.get("name"));
         System.out.println("123");
     }
 }
