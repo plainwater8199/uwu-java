5baf7f5e25f5e9dd4095c895ac2d931fd4454ab3
wangpeng <pengwanggf@isoftstone.com>
2025-04-03 17:48:52 +0800

    websocket-调试

--- uwu-study/src/main/java/com/uwu/study/netty/ws/websocketconfigurer/config/WebSocketServerConfig.java
+++ uwu-study/src/main/java/com/uwu/study/netty/ws/websocketconfigurer/config/WebSocketServerConfig.java
@@ -12,12 +12,33 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketServerConfig implements WebSocketConfigurer {
 
+    /**
+     * WebSocketHandlerRegistry  注册处理器与路径  registry.addHandler(handler, path)
+     * WebSocketHandler 处理连接/消息事件
+     * @param registry
+     */
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(myHandler(), "/my-websocket")
                 .setAllowedOrigins("*");
     }
 
+
+    /**
+     * @Bean
+     * 将返回的 WebSocketHandler 实例交给 Spring 容器管理。
+     *
+     * ServerWebSocketHandler
+     * 自定义的消息处理器类，通常继承以下类之一：
+     *
+     * TextWebSocketHandler：处理文本消息
+     *
+     * BinaryWebSocketHandler：处理二进制消息
+     *
+     * 直接实现 WebSocketHandler 接口（需重写所有方法）
+     * @return
+     */
     @Bean
     public WebSocketHandler myHandler() {
         return new ServerWebSocketHandler();
--- uwu-study/src/main/java/com/uwu/study/netty/ws/websocketconfigurer/handler/ServerWebSocketHandler.java
+++ uwu-study/src/main/java/com/uwu/study/netty/ws/websocketconfigurer/handler/ServerWebSocketHandler.java
@@ -12,6 +12,34 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class ServerWebSocketHandler extends TextWebSocketHandler {
+    /**
+     * WebSocketSession  代表一个客户端连接 session.sendMessage()
+     */
+
+
+    /**
+     * WebSocket 服务端需要同时处理：
+     *       连接建立（afterConnectionEstablished）
+     *       连接关闭（afterConnectionClosed）
+     *       消息发送（遍历 sessions 广播消息）
+     *     这些操作可能由 不同线程 同时触发（例如多个客户端同时连接/断开）。
+     *
+     *
+     * CopyOnWriteArrayList 的特性
+     * 1、写时复制：修改操作（如 add/remove）会创建新数组副本、读多写少的场景
+     * 2、线程安全：无需额外同步代码、高并发环境
+     * 3、弱一致性：迭代器遍历时能反映初始状态，不保证后续修改可见、允许短暂数据不一致
+     * 对于 WebSocket 的 sessions 管理：
+     *      写入频率：较低（仅在连接建立/断开时修改）
+     *      读取频率：极高（每次广播消息需遍历所有会话）
+     *
+     *
+     * 为什么不用 ConcurrentHashMap？
+     * 虽然 ConcurrentHashMap 也适合高并发场景，但：
+     *        列表结构更直观：会话管理通常需要遍历，而非按键查询
+     *        内存开销更小：CopyOnWriteArrayList 在元素较少时更节省内存
+     */
+
     // 线程安全的会话列表
     private static final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
